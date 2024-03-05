a45141fcafac92920a7f96edd7ba7816febd925a
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-03-05 08:25:04 -0500

    Ejercicio 6 y 7 de programas simples

---
+++ programasSimples/ejercicio6.py
@@ -0,0 +1,5 @@
+catetoA = int(input("Digite cateto A: "))
+catetoB = int(input("Digite cateto B: "))
+
+hipotenusa = (catetoA * catetoA) + (catetoB * catetoB)
+print(hipotenusa ** (0.5))
---
+++ programasSimples/ejercicio7.py
@@ -0,0 +1,6 @@
+horaActual = int(input("Digite hora actual: "))
+horas = int(input("Digite cantidad de horas: "))
+
+horaFinal = horaActual+horas
+
+print(f"En {horas} horas, el reloj marcara las {horaFinal}")
